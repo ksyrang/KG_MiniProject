8b92782373df57c395d9089eb452d26697de4296
ksyrang <95598552+ksyrang@users.noreply.github.com>
2022-02-10 10:33:33 +0900

    Package 설정

---
+++ KG_MiniProject/src/Main/Main.java
@@ -0,0 +1,28 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginMain.FXML"));
+		Parent loginForm = loader.load();
+		
+		MainController mainController = new MainController();
+		
+		primaryStage.setTitle("LoginMainPage");
+		primaryStage.setScene(new Scene(loginForm));
+		primaryStage.show();
+	}
+	
+	
+}
---
+++ KG_MiniProject/src/Main/MainController.java
@@ -0,0 +1,11 @@
+package Main;
+
+import javafx.scene.Parent;
+
+public class MainController {
+
+	
+	public void SetLoginController(Parent root) {
+		
+	}
+}

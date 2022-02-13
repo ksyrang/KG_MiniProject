2b7351e8ab95d0609c82f409c209296afd850686
SeunghoChoi1 <99311667+SeunghoChoi1@users.noreply.github.com>
2022-02-13 14:24:17 +0900

    20220213_CSH_14:23_1TeamCommitMessage수정

--- KG_MiniProject/1TeamCommitMessage.java
+++ KG_MiniProject/1TeamCommitMessage.java
@@ -2,6 +2,6 @@
 public class SYB {
 	public static void main(String[] args) {
 		System.out.println("commit-message : 20220211SYB");
-		
+		System.out.println("commit-message : 20220213CSH");
 	}
 }

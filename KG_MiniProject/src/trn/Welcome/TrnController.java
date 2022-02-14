1d860569c09942ac9f5e93979ec5f27cf9f47862
jinsoo kim <ksyrang.w@gmail.com>
2022-02-14 17:35:14 +0900

    강사 정보수정 관련 오기 수정

--- KG_MiniProject/src/trn/Welcome/TrnController.java
+++ KG_MiniProject/src/trn/Welcome/TrnController.java
@@ -1,41 +1,48 @@
 package trn.Welcome;
 
-import Main.main.Controller;
-import javafx.scene.Parent;
+import trn.EXProgramMgt.TrnExpMgtController;
+import trn.ExprogramEnroll.TrnExpEnrollController;
+import trn.TrnMgt.TrnMgtController;
 
 public class TrnController {
 
 	private TrnWelcomeController trnWelcomeController;
-	private Parent trnMgtForm;
-	private Parent trnEnrollForm;
-	private Parent trnWelcomeForm;
-	private Parent trnExPMegForm;
-//	private WelcomeService Welcomsvic;
-//	
-//	
-//	public TrnController() {
-//		Welcomsvic = new WelcomeService();
-//		Welcomsvic.setController(this);
-//	}
-	
-	public void settrnWelcomeController(Parent controller) {
-		
-	}
-	
-	public void settrnMgtForm(Parent trnMgtForm) {
-		this.trnMgtForm = trnMgtForm;
-	}
-	public void settrnEnrollForm(Parent trnEnrollForm) {
-		this.trnEnrollForm = trnEnrollForm;
-	}
-	public void settrnWelcomeForm(Parent trnWelcomeForm) {
-		this.trnWelcomeForm = trnWelcomeForm;
-	}
-	public void settrnExPMegForm(Parent trnExPMegForm) {
-		this.trnExPMegForm = trnExPMegForm;
-	}
-		
+	private TrnMgtController trnMgtController;
+	private TrnExpEnrollController trnExpEnrollController;
+	private TrnExpMgtController trnExpMgtController;
 	
 	
-
+	//웰컴 컨트롤러
+	public void setTrnWelcomeController(TrnWelcomeController trnWelcomeController) {
+		this.trnWelcomeController = trnWelcomeController;
+	}
+	public TrnWelcomeController getTrnWelcomeController() {
+		return this.trnWelcomeController;
+	}
+	
+	//강사 정보 컨트롤러
+	public void setTrnMgtController(TrnMgtController trnMgtController) {
+		this.trnMgtController = trnMgtController;
+	}
+	public TrnMgtController getTrnMgtController() {
+		return this.trnMgtController;
+	}
+	
+	//프로그램 등록 컨트롤러
+	public void setTrnExpEnrollController(TrnExpEnrollController trnExpEnrollController) {
+		this.trnExpEnrollController = trnExpEnrollController;
+	}
+	public TrnExpEnrollController getTrnExpEnrollController() {
+		return this.trnExpEnrollController;
+	}
+	
+	//프로그램 수정 컨트롤러
+	public void setTrnExpMgtController(TrnExpMgtController trnExpMgtController) {
+		this.trnExpMgtController = trnExpMgtController;
+	}
+	public TrnExpMgtController TrnExpMgtController() {
+		return this.trnExpMgtController;
+	}
+	
 }
+
--- KG_MiniProject/src/trn/Welcome/WelcomeService.java
+++ KG_MiniProject/src/trn/Welcome/WelcomeService.java
@@ -6,29 +6,73 @@ import common.CommonService;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import trn.TrnMgt.MgtDTO;
+import trn.TrnMgt.TrnMgtController;
+import trn.TrnMgt.TrnMgtService;
 
 public class WelcomeService {
 
 	private TrnController trncontroller;
+	
+	
+	private TrnMgtService trnMgtService;
+	private String trnCode;
 	private Parent WelcomeForm;
 	
-	public void setController(TrnController trncontroller) {
+	public void setTrnCode(String trnCode) {
+		this.trnCode = trnCode;
+	}
+	
+	public void setTrnController(TrnController trncontroller) {
 		this.trncontroller = trncontroller;
 	}
 	
-	public MgtDTO programclickProc(Parent programclick) {
-		// TODO Auto-generated method stub
-		return null;
+	//강사정보페이지
+	public void TrnMgtOpen() {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/trn/TrnMgt/KG_TRN_FX_Mgt.fxml"));
+		Parent trnMgtForm;
+		try {
+			trnMgtForm = loader.load();
+			trncontroller.setTrnMgtController(loader.getController());
+			trncontroller.getTrnMgtController().setTrnMgtForm(trnMgtForm);
+			trncontroller.getTrnMgtController().setTrnCode(trnCode);
+			
+			//초기 표시 설정
+//			Label IDDis = (Label)trnMgtForm.lookup("#IDDisplay");//아이디표시
+//			IDDis.setText(trnMgtService.getTrnInfo(trnCode).getTRAINER_ID());
+//			TextField NameField = (TextField)trnMgtForm.lookup("#TrnNameField");//기존 이름 표시
+//			NameField.setText(trnMgtService.getTrnInfo(trnCode).getTRAINER_Name());
+//			TextField BirthField = (TextField)trnMgtForm.lookup("#TrnBirthField");//기존 생일 표시
+//			BirthField.setText(Integer.toString(trnMgtService.getTrnInfo(trnCode).getTRAINER_Birth()));
+//			TextField MobileField = (TextField)trnMgtForm.lookup("#TrnMobileField");//기존 전번 표시
+//			MobileField.setText(Integer.toString(trnMgtService.getTrnInfo(trnCode).getTRAINER_Mobile()));
+//			TextField AddrField = (TextField)trnMgtForm.lookup("#TrnAddr1");//기존 주소 표시
+//			AddrField.setText(trnMgtService.getTrnInfo(trnCode).getTRAINER_Addr());
+//			TextField CareerField = (TextField)trnMgtForm.lookup("#TrnCareer");//기존 커리어 표시
+//			CareerField.setText(Integer.toString(trnMgtService.getTrnInfo(trnCode).getTRAINER_Career()));
+
+			Stage stage = new Stage();
+			stage.setScene(new Scene(trnMgtForm));
+			stage.setTitle("강사 정보수정 페이지");
+			stage.show();		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+	
+	
 	
 	public void ExPEnrollOpen() {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/trn/EXProgramMgt/KG_TRN_FX_EXProgramMgt.fxml"));
 		Parent trnExPEnrollFrom;
 		try {
 			trnExPEnrollFrom= loader.load();
-			trncontroller.settrnEnrollForm(trnExPEnrollFrom);
+			trncontroller.setTrnExpEnrollController(loader.getController());
 			Stage stage = new Stage();
 			stage.setScene(new Scene(trnExPEnrollFrom));
 			stage.setTitle("trnExPEnroll");
@@ -37,13 +81,16 @@ public class WelcomeService {
 			e.printStackTrace();
 		}			
 	}
-
+	//
 	public void ExPMgtOpen() {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/trn/EXProgramMgt/KG_TRN_FX_EXProgramMgt.fxml"));
 		Parent trnExPMgtFrom;
 		try {
 			trnExPMgtFrom = loader.load();
-			trncontroller.settrnEnrollForm(trnExPMgtFrom);
+			trncontroller.setTrnExpMgtController(loader.getController());
+			
+			
+			
 			Stage stage = new Stage();
 			stage.setScene(new Scene(trnExPMgtFrom));
 			stage.setTitle("trnExPEnroll");
@@ -53,8 +100,11 @@ public class WelcomeService {
 		}	
 	
 	}
+	
+	
+	
 	public void backClose(Parent back) {
-		CommonService.WindowClose(WelcomeForm);
+		CommonService.WindowClose(back);
 	}
 	
 

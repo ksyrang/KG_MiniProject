f5c859c633cd2db3bd5162c6e9e52bd2660568fb
ShinTonya <39453084+ShinTonya@users.noreply.github.com>
2022-02-15 16:57:09 +0900

    sde

--- KG_MiniProject/src/admin/memberMgt/MemberMgtDAO.java
+++ KG_MiniProject/src/admin/memberMgt/MemberMgtDAO.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import mem.Enroll.EnrollDTO;
 
 public class MemberMgtDAO {
 	private Connection con;
@@ -28,6 +29,34 @@ public class MemberMgtDAO {
 			}
 	}
 	
+	public MemberMgtDTO selectCode(String mem_code) {
+		String sql = "SELECT * FROM mem_tb WHERE mem_code= ?";
+		PreparedStatement ps;
+		ResultSet rs;
+		
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, mem_code);
+			rs = ps.executeQuery();
+			MemberMgtDTO memberMgtDto = new MemberMgtDTO();
+			if(rs.next()) {
+				memberMgtDto.setMem_code(rs.getString("mem_code"));
+				memberMgtDto.setMem_id(rs.getString("mem_id"));
+				memberMgtDto.setMem_name(rs.getString("mem_name"));
+				memberMgtDto.setMem_pw(rs.getString("mem_pw"));
+				memberMgtDto.setMem_gender(rs.getString("mem_gender"));
+				memberMgtDto.setMem_birth(rs.getString("mem_birth"));
+				memberMgtDto.setMem_mobile(rs.getString("mem_mobile"));
+				memberMgtDto.setMem_addr(rs.getString("mem_addr"));
+				memberMgtDto.setMem_approve(rs.getString("mem_approve"));
+				
+				return memberMgtDto;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 	
 	public ObservableList<MemberMgtDTO> getAllMemberList() {
 		String sql = "SELECT mem_code, mem_name, mem_approve FROM mem_tb";
--- KG_MiniProject/src/admin/memberMgt/MemberMgtService.java
+++ KG_MiniProject/src/admin/memberMgt/MemberMgtService.java
@@ -5,13 +5,16 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 
  class MemberMgtService {
 	 
 	 // @FXML private TextField idtxt, nametxt, pwtxt, mobiletxt, addrtxt;
-
+	 private MemberMgtDAO memberMgtDao;
 
 	// 필터
 	public void filter(Parent memberMgtForm) {
@@ -20,7 +23,7 @@ import javafx.scene.control.TextField;
 		String combo = comboBox.getValue();
 		
 		ObservableList<MemberMgtTable> tableView = FXCollections.observableArrayList();
-		MemberMgtDAO memberMgtDao = new MemberMgtDAO();
+		memberMgtDao = new MemberMgtDAO();
 		
 		if (combo.equals("전체보기")) {
 			System.out.println("전체보기");
@@ -49,11 +52,42 @@ import javafx.scene.control.TextField;
 	
 	// 테이블뷰 행 클릭시 이벤트
 	public void cellClick(Parent memberMgtForm, String colCode) {
+		memberMgtDao = new MemberMgtDAO();
+		MemberMgtDTO memberMgtDto = memberMgtDao.selectCode(colCode);
+		
 		TextField idfield = (TextField) memberMgtForm.lookup("#idtxt");
-		idfield.setText(colCode);
+		TextField namefield = (TextField) memberMgtForm.lookup("#nametxt");
+		TextField pwfield = (TextField) memberMgtForm.lookup("#pwtxt");
+		TextField mobilefield = (TextField) memberMgtForm.lookup("#mobiletxt");
+		TextField addrfield = (TextField) memberMgtForm.lookup("#addrtxt");
+		RadioButton mem = (RadioButton) memberMgtForm.lookup("#menradio");
+		RadioButton womem = (RadioButton) memberMgtForm.lookup("#womenradio");
+		ToggleGroup gender = new ToggleGroup();
+		mem.setToggleGroup(gender);
+		womem.setToggleGroup(gender);
 		
+		if(memberMgtDto.getMem_gender() != null) {
+			if(memberMgtDto.getMem_gender().equals("남")) {
+				mem.setSelected(true);
+			} else if(memberMgtDto.getMem_gender().equals("여")) {
+				womem.setSelected(true);
+			} 
+		}
 		
+		idfield.setText(memberMgtDto.getMem_id());
+		namefield.setText(memberMgtDto.getMem_name());
+		pwfield.setText(memberMgtDto.getMem_pw());
 		
+		if(memberMgtDto.getMem_mobile() != null) {
+			mobilefield.setText(memberMgtDto.getMem_mobile());
+		} else {
+			mobilefield.setText("정보 없음");
+		}
+		if(memberMgtDto.getMem_addr() != null) {
+			addrfield.setText(memberMgtDto.getMem_addr());
+		} else {
+			addrfield.setText("정보 없음");
+		}
 	}
 
 

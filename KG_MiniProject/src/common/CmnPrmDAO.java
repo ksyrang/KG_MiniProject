c6c8ffea3298a6125b9f071279e59472537c74c9
ksyrang <95598552+ksyrang@users.noreply.github.com>
2022-02-16 10:20:36 +0900

    KJS: CMMON PRMDAO 생성

--- KG_MiniProject/src/common/CmnPrmDAO.java
+++ KG_MiniProject/src/common/CmnPrmDAO.java
@@ -4,9 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CmnPrmDAO {
-	//TB명 : PAY_TB
+	//TB명 : PRM_TB
 	private String sql = "";
 	private Connection con = null;
 	private PreparedStatement ps = null;
@@ -23,4 +25,79 @@ public class CmnPrmDAO {
 			e.printStackTrace();
 		}
 	}
+	
+	public int IstPrm(CmnPrmDTO DTO) {
+		int result = 0;
+		sql = "INSERT INTO PRM_TB "+
+				"(PRM_Code, PRM_Name)"+
+				"VALUES(?,?)";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, DTO.getPRM_Code());
+			ps.setString(2, DTO.getPRM_Name());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try { 
+				if(ps != null) ps.close();
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public CmnPrmDTO SltPrmOne(String PRM_Code) {
+		CmnPrmDTO tmpdata = null;
+		sql = "SELECT * FROM PRM_TB WHERE PRM_Code = ?";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, PRM_Code);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				tmpdata = new CmnPrmDTO(rs.getString("PAY_CODE"), rs.getString("PAY_TYPE"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(ps != null) ps.close();
+			} catch (SQLException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+		}
+		return tmpdata;		
+	}
+	
+	public ArrayList<CmnPrmDTO> SltPrmAll(){
+		ArrayList<CmnPrmDTO> Datalist = new ArrayList<>();
+		CmnPrmDTO tmpdata = null;
+		sql = "SELECT * FROM PRM_TB" ;
+		try {
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				tmpdata = new CmnPrmDTO(rs.getString("PAY_CODE"), rs.getString("PAY_TYPE"));
+				Datalist.add(tmpdata);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(ps != null) ps.close();
+			} catch (SQLException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+		}
+		return Datalist;
+	}
+	
+	
+	
+	
 }

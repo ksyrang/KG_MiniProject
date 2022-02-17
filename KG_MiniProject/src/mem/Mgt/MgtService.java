464e83531163c62f5ff00761efeafd47bcfa7c9c
SeunghoChoi1 <99311667+SeunghoChoi1@users.noreply.github.com>
2022-02-17 10:53:11 +0900

    20220217_CSH_10:52

--- KG_MiniProject/src/mem/Mgt/MgtService.java
+++ KG_MiniProject/src/mem/Mgt/MgtService.java
@@ -1,6 +1,8 @@
 package mem.Mgt;
 
 
+import common.CmnMemDAO;
+import common.CmnMemDTO;
 import common.CommonService;
 import javafx.scene.Parent;
 import javafx.scene.control.PasswordField;
@@ -11,118 +13,60 @@ import javafx.scene.control.TextField;
 
 
 public class MgtService {
+/*	
+	public void TnrModifyProc(Parent memMgtForm, String memCode) {
+		CmnMemDAO tmpDAO = new CmnMemDAO();
+		CmnMemDTO tmpDto = new CmnMemDTO(tmpDAO.SltResOne(memCode));
+		
+		PasswordField PWField = (PasswordField)memMgtForm.lookup("#MemPWField");
+		PasswordField PWCField = (PasswordField)memMgtForm.lookup("#MemPWCField");
+		TextField NameField = (TextField)memMgtForm.lookup("#MemNameField");//변경 이름
+		TextField BirthField = (TextField)memMgtForm.lookup("#MemBirthField");//변경 생일
+		TextField MobileField = (TextField)memMgtForm.lookup("#MemMobileField");//변경 전번
+		RadioButton MaleRtn = (RadioButton)memMgtForm.lookup("#MaleRabtn");//남자버튼
+		RadioButton FeMaleRtn = (RadioButton)memMgtForm.lookup("#FeMaleRabtn");//여자버튼
+		TextField Addr1Field = (TextField)memMgtForm.lookup("#MemAddr1");//변경 주소
+		TextField Addr2Field = (TextField)memMgtForm.lookup("#MemAddr2");//변경 주소
 
-	MgtDAO mgtDAO;
-	
-	//전화번호 중복 체크
-	public void mobileConfirmProc(Parent memberMgtForm) {
-		TextField mobileTxt = (TextField) memberMgtForm.lookup("#mobileTxt");
-		String mobile = mobileTxt.getText();
-		if(mobile.isEmpty()) {
-			CommonService.Msg("전화번호를 입력해주세요.");
-		} else {
-			mgtDAO = new MgtDAO();
-			
-			if(mgtDAO.SelectMobile(Integer.parseInt(mobile)) == null) {
-				CommonService.Msg(mobile + " 은(는) 사용 가능한 전화번호입니다.");
+		//변경사항 업데이트
+		if(!PWField.getText().isEmpty() && !PWCField.getText().isEmpty()) {//정상 입력 조건
+			if(PWField.getText().equals(PWCField.getText())){//입력 값이 동일하다는 조건
+				tmpDto.setMEM_Name(NameField.getText());
+				tmpDto.setMEM_PW(PWField.getText());
+				if(MaleRtn.isSelected()) tmpDto.setMEM_Gender("남성");
+				else tmpDto.setMEM_Gender("여성");
+				tmpDto.setMEM_Birth(Integer.parseInt(BirthField.getText()));
+				tmpDto.setMEM_Mobile(Integer.parseInt(MobileField.getText()));
+				tmpDto.setMEM_Addr(Addr1Field.getText()+"/"+Addr2Field.getText());
+				int result = tmpDAO.UptTrn(tmpDto);
+				if(result == 1) {
+					CommonService.Msg("수정완료");
+					CommonService.WindowClose(memMgtForm);
+				}
+				else CommonService.Msg("이상 발생");
 			}else {
-				CommonService.Msg(mobile + " 은(는) 이미 사용하고 있는 전화번호입니다.");
-				mobileTxt.setText(null);
+				CommonService.Msg("비밀번호가 서로 다릅니다");
+				PWField.clear();
+				PWCField.clear();
+				PWField.requestFocus();
 			}
 		}
-	}
-	
-	//아이디 중복 체크
-		public void idConfirmProc(Parent memberMgtForm) {
-			TextField idTxt = (TextField) memberMgtForm.lookup("#idTxt");
-			String id = idTxt.getText();
-			if(id.isEmpty()) {
-				CommonService.Msg("ID를 입력해주세요.");
-			} else {
-				mgtDAO = new MgtDAO();
-				
-				if(mgtDAO.SelectId(id) == null) {
-					CommonService.Msg(id + " 은(는) 사용 가능한 ID입니다.");
-				}else {
-					CommonService.Msg(id + " 은(는) 이미 사용하고 있는 ID입니다.");
-					idTxt.setText(null);
-				}
-			}
+		else if(PWField.getText().isEmpty() && !PWCField.getText().isEmpty()) {
+			CommonService.Msg("비밀번호칸을 채워주세요");
 		}
-	
-	public void insert(Parent memberMgtForm) {
-		// 화면에 입력한 데이터 출력하기.
-		// System.out.println();
-		TextField nameTxt = (TextField) memberMgtForm.lookup("#nameTxt");
-		TextField idTxt = (TextField) memberMgtForm.lookup("#idTxt");
-		PasswordField pwTxt = (PasswordField) memberMgtForm.lookup("#pwTxt");
-		PasswordField confirmTxt = (PasswordField) memberMgtForm.lookup("#confirmTxt");
-		TextField birthTxt = (TextField) memberMgtForm.lookup("#birthTxt");
-		TextField mobileTxt = (TextField) memberMgtForm.lookup("#mobileTxt");
-		TextField addrTxt = (TextField) memberMgtForm.lookup("#addrTxt");
-//		Text labelTxt1 = (Text) memberJoinForm.lookup("#labelTxt1");
-//		Label colorLabel1 = (Label) KG_MEM_FX_Enroll.lookup("#colorLabel1");
-//		Label colorLabel2 = (Label) KG_MEM_FX_Enroll.lookup("#colorLabel2");
-		
-		String name = nameTxt.getText();
-		String id = idTxt.getText();
-		String pw = pwTxt.getText();
-		String confirm = confirmTxt.getText();
-		String birth = birthTxt.getText();
-		String mobile = mobileTxt.getText();
-		String addr = addrTxt.getText();
-		String approve = "false";
-//		 label1 = labelTxt1.getTextFill;
-//		colorLabel1.setTextFill(Color.RED);
-//		colorLabel2.setTextFill(Color.RED);
-//		Paint label = colorLabel.getTextFill(#e10b0b);
-		
-		
-		RadioButton manRadio = (RadioButton) memberMgtForm.lookup("#manRadio");
-		RadioButton womanRadio = (RadioButton) memberMgtForm.lookup("#womanRadio");
-
-		String gender = "";
-		if (manRadio.isSelected())
-			gender += "남";
-		else if (womanRadio.isSelected())
-			gender += "여";
-
-		if(name.isEmpty() || id.isEmpty() || pw.isEmpty() || confirm.isEmpty() || birth.isEmpty() || mobile.isEmpty() ) {
-			CommonService.Msg("필수 데이터 입니다.");
-			return;
+		else if(!PWField.getText().isEmpty() && PWCField.getText().isEmpty()) {
+			CommonService.Msg("비밀번호비교칸을 채워주세요");
 		}
-		
-		
-//		if(enrollDTOC.getMobile().equals(mobile)) {
-//			colorLabel2.setTextFill(Color.RED);
-//		}
-		
-		if(pw.equals(confirm)) {	
-			MgtDAO mgtDAO = new MgtDAO();
-			MgtDTO mgtDTO = mgtDAO.SelectId(id);
-//			System.out.println(enrollDTO);
-			if(mgtDTO == null) {
-				mgtDTO = new MgtDTO();
-				mgtDTO.setMEM_Code("mem" + id);
-				mgtDTO.setID(id);
-				mgtDTO.setPW(pw);
-				mgtDTO.setName(name);
-				mgtDTO.setGender(gender);
-				mgtDTO.setBirth(Integer.parseInt(birth));
-				mgtDTO.setMobile(Integer.parseInt(mobile));
-				mgtDTO.setAddr(addr);
-				mgtDTO.setApprove(approve);
-				
-				mgtDAO.insert(mgtDTO);
-				CommonService.Msg(id + " 계정이 등록되었습니다.");			
-			}else {
-				CommonService.Msg(id + "는/은 등록된 계정입니다.");
-				}
-		}else {
-//			colorLabel1.setTextFill(Color.RED);
-			CommonService.Msg("비밀번호가 일치하지 않습니다.");
+		else {
+			CommonService.Msg("비밀번호칸들을 채워 주세요");
 		}
 	}
+*/	
+	
+	public void BackMgtProc(Parent memMgtForm) {
+		CommonService.WindowClose(memMgtForm);
+	}
+	
 
 
 }

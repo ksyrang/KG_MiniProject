886060c169e50f732df69b0666c76564cda97462
ShinTonya <39453084+ShinTonya@users.noreply.github.com>
2022-02-17 11:18:42 +0900

    sde-CmnMemShipScheDAO

---
+++ KG_MiniProject/src/common/CmnMemShipScheDAO.java
@@ -0,0 +1,57 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CmnMemShipScheDAO {
+	private String sql = "";
+	private Connection con = null;
+	private PreparedStatement ps = null;
+	private ResultSet rs = null;
+	
+	public CmnMemShipScheDAO() {
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String user = "KGGYM";
+		String password = "oracle1";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 회원권스케줄 갯수
+	public ArrayList<CmnMemShipScheDTO> SltMemShipScheAll() {
+		ArrayList<CmnMemShipScheDTO> Datalist = new ArrayList<>();
+
+		sql = "SELECT MEMSHIPSCHE_Code FROM MEMSHIPSCHE_TB";
+		try {
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			CmnMemShipScheDTO tmpdata = new CmnMemShipScheDTO();
+			while (rs.next()) {
+				tmpdata.setMEM_Code(rs.getString("MEMSHIPSCHE_Code"));
+				Datalist.add(tmpdata);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (ps != null)
+					ps.close();
+			} catch (SQLException e2) {
+				e2.printStackTrace();
+			}
+		}
+		return Datalist;
+	}
+
+}
+

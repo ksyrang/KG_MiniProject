f9a4a91148cab5eb4d110a5fc613f4e6a46aece2
ksyrang <95598552+ksyrang@users.noreply.github.com>
2022-02-22 20:02:35 +0900

    KJS  : 주석 처리

--- KG_MiniProject/src/Main/main/Main.java
+++ KG_MiniProject/src/Main/main/Main.java
@@ -1,5 +1,8 @@
 package Main.main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import Main.login.LoginController;
 import common.CommonService;
 import common.LogOut;
@@ -20,23 +23,17 @@ public class Main extends Application{
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Main/main/KG_COM_FX_Main.fxml"));
 		Parent mainForm = loader.load();
 		Controller controller = new Controller();
-//		CommonService cmnsvc = new CommonService();
 		
 		controller.setLoginController(loader.getController());
-//		controller.getLoginController().setmainForm(mainForm);
 		LoginController loginController = controller.getLoginController();
 		loginController.setmainForm(mainForm);
 		controller.setLogOut(new LogOut());
-//		LogOut logout = new LogOut();
-//		logout.setLoginForm(mainForm);
-//		controller.setLogOut(logout);
-		
+
 		Scene scene = new Scene(mainForm);
 		
 		primaryStage.setTitle("MainPage");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
 	
 }
